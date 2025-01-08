70632ee6fff88e835caaac4da8c9c1e58a433585
SnowNightt <1006817009@qq.com>
2025-01-08 22:48:48 +0800

    feat: 完成effec函数

--- src/reactive/effect.js
+++ src/reactive/effect.js
@@ -6,12 +6,16 @@ export function effect(fn) {
       return fn();
     } finally {
       // todo
+      // fn函数调用完后，依赖收集完成，activeEffectFn可设置为undefined
+      activeEffectFn = undefined
     }
   };
   effectFn();
   return effectFn; // 不知道为啥要return
 }
 const targetMap = new WeakMap();
+// targetMap中键是代理的响应式对象，值是一个Map
+// Map中键是对象的属性，值时一个Set，里面收集依赖该属性的函数。当该属性变化时，重新触发所有函数。
 // 依赖收集
 export function track(target, prop) {
   let depsMap = targetMap.get(target);
